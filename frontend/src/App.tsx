12348ffa02a098cb4534605d933a27ae58d8de15
petar <stojkovicpetar69@gmail.com>
2025-07-08 22:12:48 +0200

    creating frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
 export default App;
